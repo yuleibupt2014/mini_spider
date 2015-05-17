a675bcd52a77a5b3d433d67189a855de4b80b524
yuleibupt2014 <yuleibupt2014@gmail.com>
2015-05-17 16:50:24 +0800

    继续删除一些不用的文件

--- testfortest.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-#coding:utf-8
-
-print "请按照以下格式输入参数行命令:"
-print "python mini_spider.py -c spider.conf [-h] [-v]'"
-print "PS:(1)其中'-c spider.conf'表示从spider.conf文件中读取配置信息"
-print "PS:(2)其中'-h'表示阅读帮助"
-print "PS:(3)其中'-c'表示显示当前版本号"
