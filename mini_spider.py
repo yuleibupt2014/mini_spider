01aa731ae4e154ee5aae1a85f00d2c7306a23d27
yuleibupt2014 <yuleibupt2014@gmail.com>
2015-05-18 18:42:12 +0800

    增加下载时Url去重

--- mini_spider.py
+++ mini_spider.py
@@ -1,31 +1,54 @@
 #!/usr/bin/python
 #coding:gbk
-
+__author__ = 'yuleibupt2014'
 
 import argparse
 import spider
 import Queue
+import os,sys
+import md5
 
+####################################################################
+url_list_file= 'C:\Users\yuleibupt2014\Desktop\mini_spider\urls'    #种子文件路径
+output_directory='C:\Users\yuleibupt2014\Desktop\mini_spider\output' #抓取结果存储目录
+max_depth= 10                       #最大抓取深度(种子为0级)
+crawl_interval= 1                  #抓取间隔. 单位: 秒
+crawl_timeout= 1                   #抓取超时. 单位: 秒
+target_url='.*.(gif|png|jpg|bmp)$ '#需要存储的目标网页URL pattern(正则表达式)
+thread_count= 8                    # 抓取线程数
+############################################################
+def main():
 
-if "__main__" == __name__:
+    global url_list_file,output_directory,max_depth,crawl_interval,crawl_timeout, target_url, thread_count
     argflag=argparse.readargs()
     if argflag=='IsOK':
         url_list_file,output_directory,max_depth,crawl_interval,crawl_timeout, target_url, thread_count=argparse.readconfigfile()
-        print url_list_file,output_directory,max_depth,crawl_interval,crawl_timeout, target_url, thread_count
     elif argflag==None:
         print '请输入命令行参数'
 
     queueUrl = Queue.Queue()
-    queueHtml = Queue.Queue()
-    queueUrl.put([0,'http://www.sina.com'])
+    dict_url = {}
 
-    t = spider.WorkerGetHtml(queueUrl, queueHtml)
-    t.setDaemon(True)
-    t.start()
+    urlfile=open(str(url_list_file),'r')       #读种子文件
+    for website in urlfile.readlines():
+        queueUrl.put([0,website])
 
-    t1 = spider.WorkerParserHtml(queueHtml, queueUrl)
-    t1.setDaemon(True)
-    t1.start()
+    # outputfileses = os.listdir(output_directory)    #????????????html???hash?
+    # for htmlfilename in outputfileses:
+    #     srcfilename = os.path.splitext(htmlfilename)[0][1:]
+    #     htmlname="http://"+srcfilename
+    #     print htmlname
+    #     url_hash = md5.new(str(htmlname)).hexdigest()
+    #     dict_url[url_hash] = str(htmlname)
+    #print dict_url
+
+    for i in range(thread_count):
+        t = spider.WorkerGetHtml(queueUrl,dict_url,max_depth)
+        t.setDaemon(True)
+        t.start()
 
     queueUrl.join()
-    queueHtml.join()
+    print len(dict_url)
+
+if __name__=='__main__':
+    main()
--- spider.py
+++ spider.py
@@ -2,54 +2,92 @@
 # -*- coding: utf-8 -*-
 __author__ = 'yuleibupt2014'
 
+import sys
+import os
 import urllib2
 import StringIO
 import gzip
 import BeautifulSoup
 import re
 import threading
+import logging
+import urlparse
+import mini_spider
+import md5
+
+htmlcount=0
+LEVELS = {
+        1:logging.CRITICAL,
+        2:logging.ERROR,
+        3:logging.WARNING,
+        4:logging.INFO,
+        5:logging.DEBUG
+    }
+level = LEVELS[5]
+logging.basicConfig(filename='log',level=level)
 
 class WorkerGetHtml(threading.Thread):
-    def __init__(self, queueUrl, queueHtml):
+    def __init__(self, queueUrl,dict_url,deep):
         threading.Thread.__init__(self)
         self.queueUrl = queueUrl
-        self.queueHtml = queueHtml
+        self.dict_url = dict_url
+        self.deep     = deep
 
     def run(self):
         while True:
             url = self.queueUrl.get()
-            print "[get] %s %s" % (url[0], url[1])
-            response = urllib2.urlopen(url[1], timeout=5)
+            url_hash = md5.new(url[1]).hexdigest()
+            if not self.dict_url.has_key(url_hash):
+                self.dict_url[url_hash] = url[1]
+                try:
+                    print "[toget] %s %s" % (url[0], url[1])
+                    response = urllib2.urlopen(url[1], timeout=5)
+                    global htmlcount
+                    htmlcount=htmlcount+1
+                    if response.info().get('Content-Encoding') == 'gzip':
+                        buf = StringIO.StringIO(response.read())
+                        f = gzip.GzipFile(fileobj=buf)
+                        html = f.read()
+                    else:
+                        html = response.read()
 
-            if response.info().get('Content-Encoding') == 'gzip':
-                buf = StringIO(response.read())
-                f = gzip.GzipFile(fileobj=buf)
-                html = f.read()
-            else:
-                html = response.read()
+                    os.chdir(mini_spider.output_directory)  #转换目录到output下
+                    print "dangqianmulu:",os.getcwd()
+                    urlnetloc=urlparse.urlparse(url[1])
+                    hostname=urlnetloc.netloc
+                    pathname=urlnetloc.path
 
-            self.queueHtml.put([url[0], html])
-            self.queueUrl.task_done()
+                    splitstring='/'
+                    addstring='~'
+                    path=str(addstring.join(pathname.split(splitstring)))
+                    desname=hostname+path+".html"
 
-class WorkerParserHtml(threading.Thread):
-    def __init__(self, queueHtml, queueUrl):
-        threading.Thread.__init__(self)
-        self.queueHtml = queueHtml
-        self.queueUrl = queueUrl
+                    print "desname:",desname
 
-    def run(self):
-        while True:
-            html = self.queueHtml.get()
-            deep = html[0] + 1
-            soup = BeautifulSoup.BeautifulSoup(html[1])
-            count = 0
-            for link in soup.findAll('a', attrs={'href': re.compile("^http://")}):
-                href = link.get('href')
-                count = count + 1
-                self.queueUrl.put([deep, href])
-                print "[href] %s %s" % (deep, href)
-            print count
-            self.queueHtml.task_done()
+                    fp = open(desname,"w+")
+                    fp.write(html)
+                    fp.close()
+                    print "haswritetimes:",str(htmlcount)
+
+                    logging.debug("{0} downloaded {1}".format(self.getName(), url[1].encode("utf8")))
+                    fp.close()
+                    print "%s [well down!hasdown] %s %s" % (self.getName(),url[0], url[1])
+                except:
+                    logging.error("Unexpected error:", sys.exc_info()[0])
+                else:
+                    if url[0] < self.deep:
+                        soup = BeautifulSoup.BeautifulSoup(html)
+                        for link in soup.findAll('a',attrs={'href': re.compile("^http://")}):
+                            href = link.get('href')
+                            self.queueUrl.put([url[0]+1, href])
+#                            logging.debug("{0} add href {1} to queue".format(self.getName(), href.encode("utf8")))
+
+                self.queueUrl.task_done() #queueUrl.task_done()，每次从queue中get一个数据之后，当处理好相关问题，最后调用该方法，以提示q.join()是否停止阻塞，让线程向前执行或者退出；
+                                             #queueUrl.join()，阻塞，直到queue中的数据均被删除或者处理。为队列中的每一项都调用一次。
+
+
+
+
 
 
 def save_html(data):
